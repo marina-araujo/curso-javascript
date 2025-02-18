45c7845891bf34cea502aa4d1b6d1cc3032e2ca2
Marina Barbosa Araujo <marina.ba@gmail.com>
2025-02-18 10:04:17 -0300

    finalizando exercicio de imc

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -15,14 +15,30 @@ form.addEventListener('submit', function (e) {
         return;
     }
     const imc = getImc(peso, altura);
-    console.log('Cheguei aqui.')
+    const nivelImc = getNivelImc(imc);
+    
+    const msg = `Seu IMC é ${imc} (${nivelImc}).`;
+
+    setResultado(msg, true);
+    //console.log(imc, nivelImc);
     // continua o código...
 });
 
-function getImc(peso, altura) {
-    const imc = peso / altura ** 2;
-    return imc.toFixed(2);
+function getNivelImc(imc) {
+    const nivel = ['Abaixo do peso', 'Peso normal', 'Sobrepeso', 'Obesidade grau 1', 'Obesidade grau 2', 'Obesidade grau 3'];
+
+    if (imc >= 39.9) return nivel[5]; //por ter somente uma linha de código interna, não precisa das chaves e como o código vai parar no primeiro if correto que ele encontrar, também não precisa do else if.
+    if (imc >= 34.9) return nivel[4];
+    if (imc >= 29.9) return nivel[3];
+    if (imc >= 24.9) return nivel[2];
+    if (imc >= 18.5) return nivel[1];
+    if (imc < 18.5) return nivel[0];
 }
+
+function getImc(peso, altura) {
+    const imc = peso / (altura * altura);
+    return imc.toFixed(2);
+};
 
 function criaP() {
     const p = document.createElement('p'); // criação de uma constante p que vai ter dentro dela um elemento tipo p (parágrafo do html)
@@ -35,6 +51,12 @@ function setResultado(msg, isValid) {
     const resultado = document.querySelector('#resultado'); // criou a constante resultado a partir do seletor da div com id resultado
     resultado.innerHTML = '';
     const p = criaP();
+
+    if (isValid) {
+        p.classList.add('paragrafo-resultado');
+    } else {
+        p.classList.add('bad');
+    }
     p.innerHTML = msg;
     resultado.appendChild(p); // insere p dentro do "pai" resultado. ou seja, insere p dentro da div resultado.
 
